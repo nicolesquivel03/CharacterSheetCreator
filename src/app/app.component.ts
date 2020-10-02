014bb8907b91bca1daba6edabc5436724833ee7e
Nicole Esquivel <nicolesquivel03@gmail.com>
2020-10-01 18:02:16 -0700

    Add toolbar and fontawesome icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFileArchive } from '@fortawesome/free-regular-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  faFileArchive = faFileArchive;
   title = 'Genesys Character Creator';
 }
